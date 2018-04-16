604918dcdc7e3ae1083c1a27b3a3062a6a11b925
artskin <artskin@163.com>
2018-04-16 15:53:29 +0800

    light

--- js/game.js
+++ js/game.js
@@ -1,5 +1,6 @@
 import * as THREE from './libs/three.module.js';
 
+THREE.BasicShadowMap;
 var Game = function(){
     this.conf = {
         bgColor:0xdddddd,
@@ -16,7 +17,11 @@ var Game = function(){
     }
     
     this.scene = new THREE.Scene()
-    // this.camera = new THREE.OrthographicCamera(
+    this.cameraPos = {
+        current: new THREE.Vector3(0, 0, 0), // 摄像机当前的坐标
+        next: new THREE.Vector3() // 摄像机即将要移到的位置
+    }
+    // this.camera2 = new THREE.OrthographicCamera(
     //     this.screen.width/-40,
     //     this.screen.width/40,
     //     this.screen.height/40,
@@ -30,45 +35,34 @@ var Game = function(){
         1,
         10000
     )
-    // this.camera.position.set( 500, 800, 1300 );
-    // this.camera.lookAt( new THREE.Vector3() );
-    // this.camera.rotation ={
-    //     x:"-77°",
-    //     y:"-12°",
-    //     z:"-45°"
-    // }
-    this.camera.setViewOffset( 
-        this.screen.width, 
-        this.screen.height, 
-        0, 
-        0, 
-        this.screen.width, 
-        this.screen.height
-    );
-    this.cameraPos = {
-        current: new THREE.Vector3(0, 0, 0), // 摄像机当前的坐标
-        next: new THREE.Vector3() // 摄像机即将要移到的位置
-    }
     this.renderer = new THREE.WebGLRenderer({antialias:true});
+    // this.camera.setViewOffset( 
+    //     this.screen.width, 
+    //     this.screen.height, 
+    //     0, 
+    //     0, 
+    //     this.screen.width, 
+    //     this.screen.height
+    // );
+    
     this.songArr = [6,9,10,9,9,9,9,9,10,5,9,5,5,5,9,9,4,5,5,5/*20*/,4,4,2,2,2,2,5,1,1,1/*30*/,3,5,5,5,5,5,5,5,5,5/*40*/,5,2,2,2,2,2,2,2,2,2/*50*/,4,4,2,2,2,2,5,2,3,3/*60*/,2,2,2,2,2,4,4,4,4,4/*70*/,4,4,4,4,5,5,5,5,5,5,10,10];
     this.cubes = [];
     this.cubeStat = {
-        nextDir:""
+        nextDir:"right"
     }
+    this.gameLight = new THREE.DirectionalLight(0xffffff, 1.1);
+    
 };
 
 Game.prototype={
     init:function(){
-        //场景渲染初始化
         var _self = this;
-        this.renderer.setSize(this.screen.width,this.screen.height);
-        this.renderer.setClearColor(this.conf.bgColor);
-        document.body.appendChild(this.renderer.domElement);
-        
-        
+        //场景渲染初始化
+        this.setScene();
         setInterval(function(){
             //_self.createCube();
         },1000)
+
         this.createCube();
         this.createCube();
         this.setLight();
@@ -81,32 +75,34 @@ Game.prototype={
         //辅助工具
         this._Helpers();
     },
-    _Helpers: function() {
-        //辅助网格
-        var helper = new THREE.GridHelper( 400, 100 );
-        //helper.setColors( 0x0000ff, 0x808080 );
-        this.scene.add( helper );       
-        var axesHelper = new THREE.AxesHelper(10)
-        this.scene.add(axesHelper)
-    },
     _render:function(){
         this.renderer.render(this.scene,this.camera);
     },
     createCube:function(){
         var _self = this;
-        var texture  = new THREE.TextureLoader().load('assets/images/tt2.jpg');
+        var texture  = new THREE.TextureLoader().load('assets/images/tt1.jpg');
         var material = new THREE.MeshBasicMaterial({map:texture});
         var geometry = new THREE.BoxGeometry(this.conf.cubeWidth, this.conf.cubeHeight, this.conf.cubeDeep,1,1,1);
         var brick    = new THREE.Mesh(geometry,material);
         if (this.cubes.length) {
-            var random = Math.random()
-            this.cubeStat.nextDir = random > 0.5 ? 'left' : 'right';
+            // var random = Math.random()
+            // this.cubeStat.nextDir = random > 0.5 ? 'left' : 'right';
             if (this.cubeStat.nextDir === 'left') {
                 brick.position.x = this.cubes[this.cubes.length - 1].position.x - 4;
             } else {
                 brick.position.z = this.cubes[this.cubes.length - 1].position.z - 4
             }
         }
+        var tween = new TWEEN.Tween(geometry.parameters)
+        .to({
+            height:6
+        })
+        .easing( TWEEN.Easing.Elastic.Out)
+        .onUpdate(function() {
+            console.log(this.height);
+        }).start();
+        var time = 1000;
+    
         this.cubes.push(brick)
         // 当方块数大于6时，删除前面的方块，因为不会出现在画布中
         if (this.cubes.length > 6) {
@@ -115,10 +111,9 @@ Game.prototype={
         //console.log(this.cubes);
         
         this.scene.add(brick);
-        //this.camera.position.z = 106;
-
         function cubeRender(){
             requestAnimationFrame(cubeRender);
+            TWEEN.update(time);
             //brick.rotation.z +=0.01;
             _self.renderer.render(_self.scene,_self.camera);
         }
@@ -131,15 +126,11 @@ Game.prototype={
     //     // cube.rotation.z +=0.01;
     // },
     setLight:function(){
-        var directionalLight = new THREE.DirectionalLight(0xffffff, 1.1);
-        directionalLight.position.set(3, 10, 5)
-        this.scene.add(directionalLight)
-
-        var light = new THREE.AmbientLight(0xffffff, 0.3)
-        this.scene.add(light)
+        this.gameLight.position.set(3, 10, 5)
+        this.scene.add(this.gameLight)
     },
     setCamera:function(){
-        this.camera.position.set(45,100,45);
+        this.camera.position.set(25,60,45);
         this.camera.lookAt(this.cameraPos.current)
     },
     _updateCameraPos: function() {
@@ -186,6 +177,23 @@ Game.prototype={
             self._updateCamera()
             })
         }
+    },
+    setScene:function(){
+        this.renderer.setSize(this.screen.width,this.screen.height);
+        this.renderer.setClearColor(this.conf.bgColor);
+        document.body.appendChild(this.renderer.domElement);
+    },
+    _Helpers: function() {
+        //辅助网格
+        var helper = new THREE.GridHelper( 600, 100 );
+        //helper.setColors( 0x0000ff, 0x808080 );
+        this.scene.add( helper );       
+        var axesHelper = new THREE.AxesHelper(100)
+        this.scene.add(axesHelper);
+        var lightHelper = new THREE.SpotLightHelper( this.gameLight );
+        this.scene.add( lightHelper );
+        var shadowCameraHelper = new THREE.CameraHelper( this.gameLight.shadow.camera );
+        this.scene.add( shadowCameraHelper );
     }
 }
 
